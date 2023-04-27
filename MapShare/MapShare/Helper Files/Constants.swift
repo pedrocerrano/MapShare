b4bbbb69e9067f8ed3740982168e4a2c9dc83688
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-04-27 14:08:13 -0500

    Establishes Modal Movement through buttons

--- MapShare/MapShare/Helper Files/Constants.swift
+++ MapShare/MapShare/Helper Files/Constants.swift
@@ -5,13 +5,31 @@
 //  Created by iMac Pro on 4/27/23.
 //
 
-import Foundation
+import UIKit
 
 struct Constants {
     
     struct Detents {
-        static let bottomDetentMultipler    = 0.09
-        static let middleDetentMultiplier   = 0.345
-        static let topDetentMultiplier      = 0.875
+//        static let bottomDetentMultipler    = 0.09
+//        static let middleDetentMultiplier   = 0.345
+//        static let topDetentMultiplier      = 0.875
+        
+        static func buildDetent(screenHeight: CGFloat) -> [UISheetPresentationController.Detent] {
+            let bottomDetent = UISheetPresentationController.Detent.custom { context in
+                screenHeight * 0.09
+            }
+            
+            let middleDetent = UISheetPresentationController.Detent.custom { context in
+                screenHeight * 0.345
+            }
+            
+            let topDetent = UISheetPresentationController.Detent.custom { context in
+                screenHeight * 0.875
+            }
+            
+            return [bottomDetent, middleDetent, topDetent]
+        }
     }
+    
+
 }
--- MapShare/MapShare/UI/Modal Home View/ModalHomeViewController.swift
+++ MapShare/MapShare/UI/Modal Home View/ModalHomeViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ModalHomeViewController: UIViewController {
-
+    
+    //MARK: - OUTLETS
+    
+    
     //MARK: - PROPERTIES
     override var sheetPresentationController: UISheetPresentationController {
         presentationController as! UISheetPresentationController
@@ -22,33 +25,51 @@ class ModalHomeViewController: UIViewController {
     }
     
     
+    //MARK: - IB ACTIONS
+    @IBAction func newMSButtonTapped(_ sender: Any) {
+        sheetPresentationController.animateChanges {
+            sheetPresentationController.selectedDetentIdentifier = sheetPresentationController.detents[2].identifier
+        }
+    }
+    
+    @IBAction func xMarkButtonTapped(_ sender: Any) {
+        sheetPresentationController.animateChanges {
+            sheetPresentationController.selectedDetentIdentifier = sheetPresentationController.detents[0].identifier
+        }
+    }
+    
+    
+    
     //MARK: - FUNCTIONS
     func configureSheetPresentationController() {
         let screenHeight = view.frame.height
-        
-        let bottomDetent = UISheetPresentationController.Detent.custom { context in
-            screenHeight * Constants.Detents.bottomDetentMultipler
-        }
-        
-        let middleDetent = UISheetPresentationController.Detent.custom { context in
-            screenHeight * Constants.Detents.middleDetentMultiplier
-        }
-        
-        let topDetent = UISheetPresentationController.Detent.custom { context in
-            screenHeight * Constants.Detents.topDetentMultiplier
-        }
-        
-        sheetPresentationController.detents = [bottomDetent, middleDetent, topDetent]
+        sheetPresentationController.detents = Constants.Detents.buildDetent(screenHeight: screenHeight)
         sheetPresentationController.prefersGrabberVisible = true
-        sheetPresentationController.largestUndimmedDetentIdentifier = topDetent.identifier
+        sheetPresentationController.largestUndimmedDetentIdentifier = sheetPresentationController.detents[2].identifier
     }
     
-
+    
+    //    func buildDetent(screenHeight: CGFloat) -> [UISheetPresentationController.Detent] {
+    //        let bottomDetent = UISheetPresentationController.Detent.custom { context in
+    //            screenHeight * Constants.Detents.bottomDetentMultipler
+    //        }
+    //
+    //        let middleDetent = UISheetPresentationController.Detent.custom { context in
+    //            screenHeight * Constants.Detents.middleDetentMultiplier
+    //        }
+    //
+    //        let topDetent = UISheetPresentationController.Detent.custom { context in
+    //            screenHeight * Constants.Detents.topDetentMultiplier
+    //        }
+    //
+    //        return [bottomDetent, middleDetent, topDetent]
+    //    }
+    
     /*
-    // MARK: - Navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-    }
-    */
-
+     // MARK: - Navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     
+     }
+     */
+    
 } //: CLASS
