c77bf4fd6214344070260eaab1b101ac747aefa4
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-05-02 13:24:50 -0500

    Adds FirebaseFirestore server to project. Updates Main Storyboard buttons

---
+++ MapShare/MapShare/Helper Files/FirebaseService.swift
@@ -0,0 +1,19 @@
+//
+//  FirebaseService.swift
+//  MapShare
+//
+//  Created by iMac Pro on 5/2/23.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct FirebaseService {
+    
+    //MARK: - PROPERTIES
+    let ref = Firestore.firestore()
+    
+    //MARK: - FUNCTIONS
+    
+    
+}
