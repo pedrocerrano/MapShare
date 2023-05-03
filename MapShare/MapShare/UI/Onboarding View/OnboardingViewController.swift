dd0074fb70cd03070fbc73e51d96f1557489e996
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-05-03 09:46:55 -0500

    Adds Onboarding Storyboard

---
+++ MapShare/MapShare/UI/Onboarding View/OnboardingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  OnboardingViewController.swift
+//  MapShare
+//
+//  Created by iMac Pro on 5/3/23.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    //MARK: - OUTLETS
+    
+    
+    //MARK: - LIFECYCLE
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+    
+    /*
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    */
+
+} //: CLASS
