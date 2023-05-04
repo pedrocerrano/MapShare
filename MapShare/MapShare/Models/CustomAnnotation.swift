423829b732b107245f90d4fcfdf6989f747a3ef1
Chase Hill <chaseaohill@gmail.com>
2023-05-04 14:21:22 -0400

    CustomAnnotation
    Created file for custom annotations.

--- MapShare/MapShare/Models/CustomAnnotation.swift
+++ MapShare/MapShare/Models/CustomAnnotation.swift
@@ -5,4 +5,18 @@
 //  Created by Chase on 5/3/23.
 //
 
-import Foundation
+import MapKit
+import CoreLocation
+
+class CustomAnnotation: NSObject, MKAnnotation {
+    
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(coordinate: CLLocationCoordinate2D, title: String, subtitle: String) {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+    }
+}
