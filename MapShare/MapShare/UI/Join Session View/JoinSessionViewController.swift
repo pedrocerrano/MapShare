96d0306685924a83d7da1c7528e4bf2747f8aaf6
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-05-08 13:42:42 -0500

    Sets up initial files and folders
    For JoinSessionView

---
+++ MapShare/MapShare/UI/Join Session View/JoinSessionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  JoinSessionViewController.swift
+//  MapShare
+//
+//  Created by iMac Pro on 5/8/23.
+//
+
+import UIKit
+
+class JoinSessionViewController: UIViewController {
+
+    //MARK: - OUTLETS
+    
+    
+    
+    //MARK: - LIFECYCLE
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+    */
+} //: CLASS
---
+++ MapShare/MapShare/UI/Join Session View/JoinSessionViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  JoinSessionViewModel.swift
+//  MapShare
+//
+//  Created by iMac Pro on 5/8/23.
+//
+
+import Foundation
+
+struct JoinSessionViewModel {
+    
+}
