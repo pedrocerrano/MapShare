6c0babbf27b33863aa090b44aa0b7faccdbfda6e
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-05-30 11:35:09 -0500

    Begins setting up location push notifications

--- MapShare/MapShare/Supporting Files/AppDelegate.swift
+++ MapShare/MapShare/Supporting Files/AppDelegate.swift
@@ -14,11 +14,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        UIApplication.shared.registerForRemoteNotifications()
         return true
     }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        #warning("self.sendDeviceTokenToServer(data: deviceToken)")
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        // Try again later
+    }
+    
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
